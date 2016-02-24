c82ea261dc76f0a4fc8600d6a78c57a01c3b00fc
kcapol1 <kcapol1@students.towson.edu>
2016-02-23 23:01:34 -0500

    Uploaded file

---
+++ MyFirstClass.java
@@ -0,0 +1,17 @@
+package de.vogella.eclipse.ide.first;
+
+public class MyFirstClass {
+/** 
+    My comments here will be put into 
+    documentation by Javadoc program 
+    @author Ken
+
+*/ 
+	public static void main(String[] args) {
+
+
+		System.out.println("Hello Eclipse!");
+
+	}
+
+}
